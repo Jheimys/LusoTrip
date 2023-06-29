b85b9e8f506fd9f5afc6a24ebf84d27e73cac509
Jheimys Bassani <jheimysbb@gmail.com>
2023-06-29 20:33:31 +0100

    :tada: Criando a estrutura inicial do projeto

---
+++ src/componets/Header/index.js
@@ -0,0 +1,22 @@
+import logo from "../tuga.png"
+import search from "../search.png"
+
+import"./header.css"
+
+const Header = () => {
+    return(
+        <header className="header" >
+            <img src={logo} alt="Logo euro tuga"/>
+
+            <div className="header__conatiner">
+                <input type='text' placeholder="O que você procura?" />
+                <img src={search} alt='lupa' />
+            </div>
+
+        </header>
+
+        
+    )
+}
+
+export default Header
